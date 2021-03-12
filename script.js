d50ff5c36b7c43a72ca1706153d9106a6f3542b1
pablogonzartist <gonzp390@newschool.edu>
2021-03-12 00:39:45 -0500

    Simple Javascript + Website

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('#question');
+
+const questionEl = document.getElementById('question');
+
+const answerEL = document.getElementById ('answers')
+
+const name = prompt('Who is your favorite singer?');
+
+questionEl.innerHTML = name;
+
+if (answers) {
+    answers.classList.remove('hidden');
+ }
+
+
